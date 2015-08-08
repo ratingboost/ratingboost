48c6a8ec894073374a6b148ed166e53fc29715f8
Justin P. Oberg <justin.oberg@icloud.com>
2015-08-07 20:40:09 -0700

    First commit

---
+++ db/migrate/20150808022807_create_businesses.rb
@@ -0,0 +1,12 @@
+class CreateBusinesses < ActiveRecord::Migration
+  def change
+    create_table :businesses do |t|
+      t.string :name
+      t.string :address
+      t.string :phone
+      t.string :website
+
+      t.timestamps
+    end
+  end
+end
